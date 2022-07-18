c0f0f62ad34e805391428a7efc29749428799361
vandung93 <dungnguyenvanitnews@gmail.com>
2022-07-18 10:55:22 +0700

    Update Style

--- gulp/paths.js
+++ gulp/paths.js
@@ -1,10 +1,12 @@
 'use strict';
 var glob      = require( 'glob' ),
     files     = glob( 'src/*', { sync: true } ),
-    mainTheme = files[ 0 ].replace( 'src/', '' );
+    mainTheme = files[ 0 ].replace( 'src/', '' ),
+    litepaper = mainTheme + '/litepaper';
 
 module.exports = {
 	mainTheme: mainTheme,
+	litepaper: mainTheme + '/litepaper',
 	taskDone: [
 		'src/**/*.php',
 		'gulp/**/*.js'
@@ -14,7 +16,11 @@ module.exports = {
 	},
 	javascript: {
 		src: 'src/' + mainTheme + '/assets/js-code/**/*.js',
-		dist: 'src/' + mainTheme + '/assets/js/'
+		dist: 'src/' + mainTheme + '/assets/js/',
+		litepaper: {
+			src: 'src/' + litepaper + '/assets/js-code/**/*.js',
+			dist: 'src/' + litepaper + '/assets/js/'
+		}
 	},
 	sass: {
 		watch: [
@@ -23,7 +29,16 @@ module.exports = {
 		generate: [
 			'src/' + mainTheme + '/assets/scss/*.scss'
 		],
-		dist: 'src/' + mainTheme + '/assets/css/'
+		dist: 'src/' + mainTheme + '/assets/css/',
+		litepaper: {
+			watch: [
+				'src/' + litepaper + '/assets/scss/**/*.scss'
+			],
+			generate: [
+				'src/' + litepaper + '/assets/scss/*.scss'
+			],
+			dist: 'src/' + litepaper + '/assets/css/',
+		}
 	},
 	bs: {
 		main: [
@@ -32,6 +47,12 @@ module.exports = {
 			'src/' + mainTheme + '/assets/js/*.js',
 			'src/' + mainTheme + '/assets/libs/**/**/*.js'
 		],
+		litepaper: [
+			'src/' + litepaper + '/*.html',
+			'src/' + litepaper + '/assets/css/*.css',
+			'src/' + litepaper + '/assets/js/*.js',
+			'src/' + litepaper + '/assets/libs/**/**/*.js'
+		]
 	},
 	linting: {
 		js: 'src/' + mainTheme + '/assets/js-code/',
--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -36,3 +36,35 @@ gulp.task( 'sass:full', function() {
 		           sourceRoot: '../scss/'
 	           } ) );
 } );
+
+// Build SASS.
+gulp.task( 'sass:litepaper', function() {
+	return gulp.src( paths.sass.litepaper.generate )
+	           .pipe( $.plumber( { errorHandler: reportError } ) )
+	           .pipe( $.sourcemaps.init() )
+	           .pipe( $.sass() )
+	           .pipe( $.sourcemaps.write( 'sourcemap/', {
+		           includeContent: false,
+		           sourceRoot: '../scss/'
+	           } ) )
+	           .pipe( $.lineEndingCorrector() )
+	           .pipe( gulp.dest( paths.sass.litepaper.dist ) );
+} );
+
+// Build SASS final.
+gulp.task( 'sass:full:litepaper', function() {
+	return gulp.src( paths.sass.litepaper.generate )
+	           .pipe( $.plumber( { errorHandler: reportError } ) )
+	           .pipe( $.sourcemaps.init() )
+	           .pipe( $.sass() )
+	           .pipe( $.postcss( processors.modules ) )
+	           .pipe( $.stripCssComments() )
+	           .pipe( $.cssnano( processors.nano ) )
+	           .pipe( $.lineEndingCorrector() )
+	           .pipe( gulp.dest( paths.sass.litepaper.dist ) )
+	           .pipe( $.sourcemaps.write( 'sourcemap/', {
+		           addComment: false,
+		           includeContent: false,
+		           sourceRoot: '../scss/'
+	           } ) );
+} );
